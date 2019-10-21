77e469cde59066b333a587ecc8fa9996c4496b90
Jeremy Fleshman <jfleshman@gmail.com.com>
2019-10-21 14:22:22 -0500

    Initial Commit

---
+++ SwiftUI_Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Todo
+//
+//  Created by Jeremy Fleshman on 10/21/19.
+//  Copyright © 2019 Jeremy Fleshman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
